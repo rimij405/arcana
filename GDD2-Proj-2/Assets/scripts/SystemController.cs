163c9f854cba9592c793651be14d6cacf04d878a
Chris <cak1352@g.rit.edu>
2017-10-04 22:46:08 -0400

    Commiting some project files and some base classes

---
+++ GDD2-Proj-2/Assets/scripts/SystemController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UI.UIManager;
+using Game.GameManager;
+using Input.InputManager;
+using System.Collections;
+
+/**
+ *  public class SystemController handles the 
+ * 
+ **/
+public class SystemController : MonoBehaviour {
+
+    // Create references to the UIManager, GameManager, and InputManager.
+    UIManager _uiManager;
+    GameManager _gameManager;
+    InputManager _inputManager;
+
+	// Initialize the ui, game, and input manager objects.
+	void Start () {
+
+        this._uiManager = new UIManager();
+        this._gameManager = new GameManager();
+        this._inputManager = new InputManager();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
