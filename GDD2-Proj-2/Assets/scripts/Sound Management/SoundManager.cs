5f8b29864b82c6344db22506af75fb01c1883e4d
Chris <cak1352@g.rit.edu>
2017-10-08 14:28:56 -0400

    Adding all screen classes and their abstracted methods and fields.

---
+++ GDD2-Proj-2/Assets/scripts/Sound Management/SoundManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sound.SoundManager
+{
+    class SoundManager
+    {
+    }
+}
---
+++ GDD2-Proj-2/Assets/scripts/UI Management/States/Screens/SplashScreen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UI.UIManagement
+{
+    /**
+     * class SplashScreen is the screen that is displayed at game startup.
+     * It will say something along the lines of "Press any key to continue"
+     * and then bring up the main menu screen.
+     **/
+    class SplashScreen : Screen
+    {
+        // Public class references
+        public StateManager m_manager { get; set; }
+
+        // Public data members
+        public bool m_initialized { get; set; }
+        public bool m_loaded { get; set; }
+        public List<int> m_stateIDs { get; set; }
+        public int m_screenResolutionX { get; set; }
+        public int m_screenResolutionY { get; set; }
+
+        /**
+         * void initialize will be called to initialize this screen object.
+         **/
+        public void initialize()
+        {
+
+        }
+
+        /**
+         * void Load will load this screen object to the stage
+         **/
+        public void Load()
+        {
+
+        }
+
+        /**
+         * Update is called every frame to update the state of the stage.
+         **/
+        public void Update(float delta)
+        {
+
+        }
+    }
+}
